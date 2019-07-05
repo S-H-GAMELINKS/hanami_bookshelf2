9cf21508de482853949f786dcfe930d01c403767
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-05 10:17:08 +0900

    add include Web::View

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -2,10 +2,7 @@ module Web
     module Controllers
       module Home
         class Index
-          include Web::Action
-  
-          def call(params)
-          end
+            include Web::View
         end
       end
     end
